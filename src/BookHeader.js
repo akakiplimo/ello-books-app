a37271b6353c62be2c3fda6c66c39f30a8821640
Abu Kiplimo <akakiplimo@gmail.com>
2022-04-29 10:04:07 +0300

    Book Header edit

--- src/BookHeader.js
+++ src/BookHeader.js
@@ -1,7 +1,7 @@
 const BookHeader = ({ book }) => {
     return (
       <div className="text-center">
-        <h1>Ello Technology Books</h1>
+        <h1>Hello Ello Books</h1>
         <h4>Book Title: {book.title}</h4>
         <p>
           <em>Book Author: {book.author}</em>
